356cc97db13afc393c1172d272b82d26ba99d2c8
Marcelo Brosowicz <marcelo.brosowicz@signaedu.com>
2017-11-20 01:17:42 -0200

    Little changes and updates

---
+++ TrabalhoFinal/src/Elemento.java
@@ -0,0 +1,66 @@
+public class Elemento {
+
+    String nome;
+    String cargo;
+    int matricula;
+    String curso;
+    String cidadeOrigem;
+    double salario;
+
+    public Elemento(String nome, String cargo, int matricula, String curso, String cidadeOrigem, double salario) {
+        this.cargo = cargo;
+        this.nome = nome;
+        this.matricula = matricula;
+        this.curso = curso;
+        this.cidadeOrigem = cidadeOrigem;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getCidadeOrigem() {
+        return cidadeOrigem;
+    }
+
+    public void setCidadeOrigem(String cidadeOrigem) {
+        this.cidadeOrigem = cidadeOrigem;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
